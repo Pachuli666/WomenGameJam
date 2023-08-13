be58299e7fbee9a1a161632ad5964533ed615da8
Arturo Granados <welcometothegrayzone@gmail.com>
2023-08-12 21:54:35 -0600

    MOVIMIENTO FINAL PERSONAJE

--- Assets/Code/playerController.cs
+++ Assets/Code/playerController.cs
@@ -2,54 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
 {
-    //ESTE SE USA PARA NO USAR RIGIDBODY Y PODER MOVER UN OBJETO
-    private CharacterController controller;
+    public float speed = 3;
+    public float rotationSpeed = 90;
+    public float gravity = -20f;
+    public float jumpSpeed = 15;
 
-    //VELOCIDAD DE MOVIMIENTO HORIZONTAL Y VERTICAL
-    public float walkSpeed = 5f;
+    CharacterController characterController;
+    Vector3 moveVelocity;
+    Vector3 turnVelocity;
 
-    //VELOCIDAD DE GIRO
-    public float turnSpeed = 180f;
-
-    void Start()
+    void Awake()
     {
-       controller = GetComponent<CharacterController>();    
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        Vector3 movDir;
+        var hInput = Input.GetAxis("Horizontal");
+        var vInput = Input.GetAxis("Vertical");
 
-        //AQUI LE DECIMOS QUE CADA VEZ QUE GIRE EN SU PROPIO EJE LO HAGO CON LA VELOCIDAD DE LA VARIABLE TURNSPEED
-        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
-
-        //EL JUGADOR PUEDE DESPLAZARSE HACIA LOS LADOS
-        movDir = transform.forward * Input.GetAxis("Vertical") * walkSpeed;
-        
-        //CONFIGURAMOS EL MOVIMIENTO TOTAL Y LA POSIBILIDAD DE SALTAR
-        controller.Move(movDir * Time.deltaTime - Vector3.up * 0.1f);
-
-
-        
-        //float buttonHorizontal = Input.GetAxis("Horizontal");
-        //float buttonVertical = Input.GetAxis("Vertical") ;
-         
-        //if(buttonHorizontal != 0){
-        //      rb.velocity = new Vector3 (playerVelocity* buttonHorizontal, rb.velocity.y, rb.velocity.z);
-        
-        //}else{
-        //     rb.velocity = new Vector3 (0, rb.velocity.y, rb.velocity.z);
-        //}
-        
-        //if(buttonVertical != 0){
-        //rb.velocity = new Vector3 (rb.velocity.x, rb.velocity.y , playerVelocity * buttonVertical);
-        //}else{
-
-        // rb.velocity = new Vector3 (rb.velocity.x, rb.velocity.y, 0);
-        //}
-
+        if (characterController.isGrounded)
+        {
+            moveVelocity = transform.forward * speed * vInput;
+            turnVelocity = transform.up * rotationSpeed * hInput;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveVelocity.y = jumpSpeed;
+            }
+        }
+        //Adding gravity
+        moveVelocity.y += gravity * Time.deltaTime;
+        characterController.Move(moveVelocity * Time.deltaTime);
+        transform.Rotate(turnVelocity * Time.deltaTime);
     }
 }
